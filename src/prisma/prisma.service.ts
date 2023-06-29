89c9fb180905de20f874df18caed9980a619d2f2
Hypermario <molvautj@gmail.com>
2023-06-29 15:23:39 +0200

    prisma services

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,10 +2,10 @@ import { INestApplication, Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from '@prisma/client'
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-	async onModuleInit() {
-		await this.$connect();
-	}
+export class PrismaService extends PrismaClient { // implements OnModuleInit {
+	// async onModuleInit() {
+	// 	await this.$connect();
+	// }
 
 	async enableShutdownHooks(app: INestApplication) {
 		this.$on('beforeExit', async () => {
