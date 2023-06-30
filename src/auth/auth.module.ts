cc0cfb42b69d20582cc5f0e16bae182af2c39f9b
Hypermario <molvautj@gmail.com>
2023-06-30 19:03:28 +0200

    no POST login

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+// export const jwtSecret = ;
+
+@Module({
+  imports: [
+    ConfigModule,
+    PrismaModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: '5m' }
+        }
+      },
+      inject: [ConfigService]
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService]
+})
+export class AuthModule {}
