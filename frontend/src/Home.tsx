ffcbc7ef2b9fb56cf5dfde125e39a281b934a679
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-07-05 11:02:29 +0200

    putting login button in a 3-layers sliding cubs

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -4,8 +4,12 @@ import './App.css'
 function Home() {
   return (
     <div>
-      <h1>Page d'accueil</h1>
-      <Link to="/login">Se connecter</Link>
+      <h1>Pooong?</h1> <br></br>
+      <ul>
+        <li style={{ "--i": 2}}><a href="#">Don't click me</a></li>
+        <li style={{ "--i": 1}} ><a href="#">Nope, still not</a></li>
+        <li style={{ "--i": 0}}><Link to="/login">Login</Link></li>
+    </ul>
     </div>
   );
 }
