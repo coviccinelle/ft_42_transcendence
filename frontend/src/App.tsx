926ee408c93e9b1e8d1b5b455bde6e5b854e2e03
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-07-07 11:50:42 +0200

    wip : adding sticker js scrip to have a circle + not working yet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,9 +13,28 @@ function App() {
   )
 }
 
-// function myFunction() {
-//   let element = document.body;
-//   element.classList.toggle("dark-mode");
-// }
+$(document).ready(function() {
+  var containerWidth = $(window).width(); // Width of the container element (e.g., the window)
+  var containerHeight = $(window).height(); // Height of the container element
+
+  var radius = Math.min(containerWidth, containerHeight) / 2; // Radius of the circle
+
+  var stickers = $('.sticker'); // Select all the sticker elements
+
+  var angle = (2 * Math.PI) / stickers.length; // Calculate the angle between stickers
+
+  stickers.each(function(index) {
+    var currentAngle = index * angle;
+
+    var xPos = containerWidth / 2 + radius * Math.cos(currentAngle) - $(this).width() / 2;
+    var yPos = containerHeight / 2 + radius * Math.sin(currentAngle) - $(this).height() / 2;
+
+    $(this).css({
+      left: xPos,
+      top: yPos
+    });
+  });
+});
+
 
 export default App
