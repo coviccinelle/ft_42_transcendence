c062985a1486019ee5a2abf46fc2b1d5c54d88c5
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-07-08 14:56:50 +0200

    semi merge /copying from front end

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Login from './Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Home'
+import Login from './Login'
 
 function App() {
   return (
@@ -10,7 +10,31 @@ function App() {
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+$(document).ready(function() {
+  var containerWidth = $(window).width(); // Width of the container element (e.g., the window)
+  var containerHeight = $(window).height(); // Height of the container element
+
+  var radius = Math.min(containerWidth, containerHeight) / 2; // Radius of the circle
+
+  var stickers = $('.sticker'); // Select all the sticker elements
+
+  var angle = (2 * Math.PI) / stickers.length; // Calculate the angle between stickers
+
+  stickers.each(function(index) {
+    var currentAngle = index * angle;
+
+    var xPos = containerWidth / 2 + radius * Math.cos(currentAngle) - $(this).width() / 2;
+    var yPos = containerHeight / 2 + radius * Math.sin(currentAngle) - $(this).height() / 2;
+
+    $(this).css({
+      left: xPos,
+      top: yPos
+    });
+  });
+});
+
+
+export default App
--- vite/src/Home.tsx
+++ vite/src/Home.tsx
@@ -1,11 +1,15 @@
-import { Link } from 'react-router-dom';
-import './App.css';
+import { Link } from "react-router-dom";
+import './App.css'
 
 function Home() {
   return (
     <div>
-      <h1>Page d'accueil</h1>
-      <Link to="/login">Se connecter</Link>
+      <h1>Pooong?</h1> <br></br>
+      <ul>
+        <li style={{ "--i": 2}}><a href="#">Don't click me</a></li>
+        <li style={{ "--i": 1}} ><a href="#">Nope, still not</a></li>
+        <li style={{ "--i": 0}}><Link to="/login">Login</Link></li>
+      </ul>
     </div>
   );
 }
