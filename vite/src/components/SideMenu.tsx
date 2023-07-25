dffbb1af5ae84e4b1b63a2aa57b3a47afb05011a
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-07-25 14:34:20 +0200

    burger

--- vite/src/components/SideMenu.tsx
+++ vite/src/components/SideMenu.tsx
@@ -4,10 +4,30 @@ import { RiPingPongLine } from 'react-icons/ri';
 
 
 function General(): JSX.Element {
+    const clickedStyle = "flex flex-1 items-center space-x-3 rounded-md bg-gray-700 px-2 py-1.5 text-sm font-medium text-white";
+    const unclickedStyle = "flex flex-1 items-center space-x-3 rounded-md hover:bg-gray-700 px-2 py-1.5 text-sm font-medium text-white";
+    const [homeStyle, setHomeStyle] = useState(clickedStyle);
+
+    const handleClick = () => {
+        setHomeStyle(clickedStyle);
+    };
+
     return (
-        <div className="flex flex-col items-center">
-            <div className="text-2xl font-bold">General</div>
-            <div className="text-xl">General</div>
-        </div>
+        <ul>
+            <li>
+            <a onClick={homeClick} className={homeStyle}>
+                <IoMdHome />
+                <span className="flex-1">Home</span>
+            </a>
+            </li>
+        </ul>
     );
 }
+
+interface SMButtonProps {
+    href: string;
+    children: string | JSX.Element | JSX.Element[];
+}
+
+function SMButton(props: SMButtonProps): JSX.Element {
+    return ()
