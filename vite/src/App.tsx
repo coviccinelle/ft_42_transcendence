c3f3d92a266e2e8c2762662f51a4bb492cd01463
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-07-31 16:13:56 +0200

    blank for now, needed to be completely cleaner : all in app.tsx and all will just be components:

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import React, { useState } from 'react';
-import Home from './Home';
+import React, { useState, useEffect } from 'react';
+// import Home from './Home';
 import Login from './Login';
 import Profile from './Profile';
 import './App.css';
@@ -8,41 +8,16 @@ import './App.css';
 function App() {
   const [darkMode, setDarkMode] = useState(false);
 
-  //function toggleDarkMode
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-    document.documentElement.classList.toggle('dark');
-  };
+
 
   return (
-    <div className={`container mx-auto p-4 ${darkMode ? 'dark' : ''}`}>
-      <div className="flex iteam-center justify-center mb-4">
-        <span role="img" aria-label='night' className="text-2x1 mr-2">
-          🌙
-        </span>
-        <label htmlFor='darkModeToggle' className="cursor-pointer">
-          Dark Mode
-        </label>
-        <input
-          type='checkbox'
-          id='darkModeToggle'
-          className='hidden'
-          onChange={toggleDarkMode}
-          checked={darkMode}
-        />
-        <span role="img" aria-label='sun' className="text-2x1 ml-2">
-        🌻
-        </span>
-      </div>
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
