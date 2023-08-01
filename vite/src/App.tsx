1fa8947b44abdedf839860c6cad54dec9e1ab00b
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-08-01 18:15:34 +0200

    popped darkmode on top + dark mode proprely implemented

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -28,7 +28,8 @@ function App() {
 
   return (
     <div>
-    <div className="top-4 fixed right-4 p-2 rounded-full cursor-pointer dark:bg-yellow-200 bg-gray-600">
+    <div style={{zIndex: 9999}} //putting the dark mode toggle on top of everything
+    className="top-4 fixed right-4 p-2 rounded-full cursor-pointer dark:bg-yellow-200 bg-gray-600">
       <span
         role="img"
         aria-label="dark mode"
