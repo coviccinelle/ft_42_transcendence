ccb9fa20d31b8e20742c65e50fb6a45d86a99f13
coviccinelle_mac <thi-phng@student.42.fr>
2023-08-14 23:50:26 +0200

    okay log in, sign in to go, should be fast

--- vite/src/pages/Login.tsx
+++ vite/src/pages/Login.tsx
@@ -11,7 +11,8 @@ function Login() {
   const [lastName, setLastName] = useState('');
   const navigate = useNavigate();
 
-  const handleSignup = () => {
+  const handleSignup = (event: any) => {
+    event.preventDefault();
     const formData = {
       firstName: firstName,
       lastName: lastName,
@@ -31,7 +32,8 @@ function Login() {
       });
   };
 
-  const handleLogin = () => {
+  const handleLogin = (event: any) => {
+    event.preventDefault();
     const formData = {
       email: email,
       password: password,
@@ -68,7 +70,7 @@ function Login() {
 			<input type="password" name="" required=""/>
 			<label>Password</label>
 		</div>
-		<a onClick={handleLogin}>
+		<a type='submit' onClick={handleLogin}>
 			<span></span>
 			<span></span>
 			<span></span>
@@ -76,56 +78,12 @@ function Login() {
 			Log in
 		</a>
     <br></br>
-      <button onClick={handleSignup}>Signup</button>
-      <button onClick={handleLoginGoogle}>Google</button>
-      <button onClick={handleLoginFt}>42</button>
+    <button type='submit' onClick={handleSignup}>Signup</button>
 		</form>
+      <button type='button' onClick={handleLoginGoogle}>Google</button>
+      <button type='button' onClick={handleLoginFt}>42</button>
 	</div>
   );
 }
-
-//     <div className="flex flex-col items-center justify-center h-screen bg-gray-00">
-//       <div className="flex flex-col items-center justify-center w-96 h-96 bg-slate-400 dark:bg-slate-500 rounded-xl shadow-xl">
-//         <div className="flex flex-col items-center justify-center h-96 w-full ">
-//           <h1 className="text-4xl font-bold my-7">Login</h1>
-//           <div className="flex flex-col items-center justify-center w-full h-1/2">
-//             <input
-//               className="w-3/4 h-15 px-3 mb-3 placeholder-gray-500 border rounded-lg focus:shadow-outline"
-//               type="text"
-//               placeholder="Email"
-//               onChange={(e) => setEmail(e.target.value)}
-//             />
-//             <input
-//               className="w-3/4 h-15 px-3 mb-3 placeholder-gray-500 border rounded-lg focus:shadow-outline"
-//               type="password"
-//               placeholder="Password"
-//               onChange={(e) => setPassword(e.target.value)}
-//             />
-//             <button
-//               className="w-3/4 h-10 px-3 mb-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-//               onClick={handleLogin}
-//             >
-//               Login
-//             </button>
-//             <button
-//               className="w-3/4 h-10 px-3 mb-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-//               onClick={handleSignup}
-//             >
-//               Sign up
-//             </button>
-
-//             {/* <button
-//               className="w-3/4 h-10 px-3 mb-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-//               onClick={handleLoginGoogle}
-//             >
-//               Login with Google
-//             </button> */}
-//             {/* <button
-//               className="w-3/4 h-10 px-3 mb-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-//               onClick={handleLoginFt}
-//             >
-//               Login with 42
-//             </button> */}
-//           </div>
 
 export default Login;
