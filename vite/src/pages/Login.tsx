5c79a40742dfcf8bf1f9addb241b7694bdfce022
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-08-17 15:28:29 +0200

    log in and Sign up pretty much okay

--- vite/src/pages/Login.tsx
+++ vite/src/pages/Login.tsx
@@ -38,35 +38,42 @@ function Login() {
 
   return (
     <div className="login-box">
-		<h1 className="my-9 animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black">Login</h1>
-		
-    <form>
-		<div className="user-box">
-			<input type="email" value={email}
-        onChange={(e) => setEmail(e.target.value)}/>
-			<label>Email</label>
-		</div>
-		<div className="user-box">
-			<input type="password" value={password}
-        onChange={(e) => setPassword(e.target.value)}/>
-			<label>Password</label>
-		</div>
-		<a type='submit' onClick={handleLogin}>
-			<span></span>
-			<span></span>
-			<span></span>
-			<span></span>
-			Log in
-		</a>
-    <br></br>
+      <h1 className="mb-9 py-2 animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black">
+        Login</h1>
 
-		</form>
-      
-      <button className="mx-3 my-3" type='button' onClick={handleLoginGoogle}>Google</button>
-      <button className="mx-3 my-3" type='button' onClick={handleLoginFt}>42</button>
+      <form>
+        <div className="user-box">
+          <input type="email" value={email}
+            onChange={(e) => setEmail(e.target.value)} />
+          <label>Email</label>
+        </div>
+        <div className="user-box">
+          <input type="password" value={password}
+            onChange={(e) => setPassword(e.target.value)} />
+          <label>Password</label>
+        </div>
+        <a type='submit' onClick={handleLogin}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Log in
+        </a>
+        <br></br>
+
+      </form>
+
+      <button className="mx-3 px-2 py-2 rounded-md border border-transparent cursor-pointer hover:border-amber-200 focus:outline-4 focus:ring-amber-500 focus:border-amber-500 focus:ring-opacity-50"
+        type='button' onClick={handleLoginGoogle}>Google</button>
+      <button className="mx-3 px-2 py-2 rounded-md border border-transparent cursor-pointer hover:border-amber-200 focus:outline-4 focus:ring-amber-500 focus:border-amber-500 focus:ring-opacity-50"
+        type='button' onClick={handleLoginFt}>42</button>
       <br></br>
-      <a className='object-right mx-3 my-3' href='/signup'>Sign up</a>
-	</div>
+      <div className="mt-12 flex items-center">
+        <p className="mr-20">Don't have an account?</p>
+        <a href="/signup" className="py-1 px-1 text-blue-500">Sign Up</a>
+      </div>
+
+    </div>
   );
 }
 
--- vite/src/pages/SignUp.tsx
+++ vite/src/pages/SignUp.tsx
@@ -41,42 +41,54 @@ function SignUp() {
   };
 
   return (
-    <div className="login-box">
-		<h1 className="my-9 animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black">Sign Up</h1>
-		<form>
-    <div className="user-box">
-			<input type="firstName" value={firstName}
-        onChange={(e) => setFirstName(e.target.value)}/>
-			<label>First Name</label>
-		</div>
-    <div className="user-box">
-			<input type="lastName" value={lastName}
-        onChange={(e) => setLastName(e.target.value)}/>
-			<label>Last Name</label>
-		</div>
-		<div className="user-box">
-			<input type="email" value={email}
-        onChange={(e) => setEmail(e.target.value)}/>
-			<label>Email</label>
-		</div>
-		<div className="user-box">
-			<input type="password" value={password}
-        onChange={(e) => setPassword(e.target.value)}/>
-			<label>Password</label>
-		</div>
-		<a type='submit' onClick={handleSignup}>
-			<span></span>
-			<span></span>
-			<span></span>
-			<span></span>
-			Sign Up
-		</a>
-    <br></br>
-    <a href='/login'>Log in</a>
-		</form>
-      <button className="mx-3 my-3" type='button' onClick={handleLoginGoogle}>Google</button>
-      <button className="mx-3 my-3" type='button' onClick={handleLoginFt}>42</button>
-	</div>
+    <>
+      <div className="login-box">
+        <h1 className="mb-9 py-2 animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black">
+          Sign Up</h1>
+        <form>
+          <div className="user-box">
+            <input type="firstName" value={firstName}
+              onChange={(e) => setFirstName(e.target.value)} />
+            <label>First Name</label>
+          </div>
+          <div className="user-box">
+            <input type="lastName" value={lastName}
+              onChange={(e) => setLastName(e.target.value)} />
+            <label>Last Name</label>
+          </div>
+          <div className="user-box">
+            <input type="email" value={email}
+              onChange={(e) => setEmail(e.target.value)} />
+            <label>Email</label>
+          </div>
+          <div className="user-box">
+            <input type="password" value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+            <label>Password</label>
+          </div>
+          <a type='submit' onClick={handleSignup}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Sign Up
+          </a>
+          {/* <a className='object-right mx-3 my-3' href='/signup'>Sign up</a> */}
+
+        </form>
+
+        <button className="mx-3 px-2 py-2 rounded-md border border-transparent cursor-pointer hover:border-amber-200 focus:outline-4 focus:ring-amber-500 focus:border-amber-500 focus:ring-opacity-50"
+          type='button' onClick={handleLoginGoogle}>Google</button>
+        <button className="mx-3 px-2 py-2 rounded-md border border-transparent cursor-pointer hover:border-amber-200 focus:outline-4 focus:ring-amber-500 focus:border-amber-500 focus:ring-opacity-50"
+          type='button' onClick={handleLoginFt}>42</button>
+        <br></br>
+        <div className="mt-12 flex items-center">
+          <p className="mr-16">Already have an account?</p>
+          <a href="/login" className="py-1 px-1 text-blue-500">Log in</a>
+        </div>
+
+      </div>
+    </>
   );
 }
 
