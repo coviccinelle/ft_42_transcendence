f632798318024fe1aeff92c191fcbdc557d2259e
coviccinelle_mac <thi-phng@student.42.fr>
2023-08-21 19:21:17 +0200

    WiP profile, init a new set of profile

--- vite/src/pages/Profile.tsx
+++ vite/src/pages/Profile.tsx
@@ -45,13 +45,13 @@ function Profile() {
           <div className="flex flex-wrap justify-center">
             <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
               <div className="relative">
-                <img alt="..." src="https://demos.creative-tim.com/notus-js/assets/img/team-2-800x800.jpg" className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"/>
+                <img alt="..." src={img} className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"/>
               </div>
             </div>
             <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
               <div className="py-6 px-3 mt-32 sm:mt-0">
                 <button className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
-                  Connect
+                  Add Friend
                 </button>
               </div>
             </div>
@@ -61,38 +61,36 @@ function Profile() {
                   <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">22</span><span className="text-sm text-blueGray-400">Friends</span>
                 </div>
                 <div className="mr-4 p-3 text-center">
-                  <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">10</span><span className="text-sm text-blueGray-400">Photos</span>
+                  <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">10</span><span className="text-sm text-blueGray-400">Matches</span>
                 </div>
                 <div className="lg:mr-4 p-3 text-center">
-                  <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">89</span><span className="text-sm text-blueGray-400">Comments</span>
+                  <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">#8</span><span className="text-sm text-blueGray-400">Rank</span>
                 </div>
               </div>
             </div>
           </div>
           <div className="text-center mt-12">
             <h3 className="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
-              Jenna Stones
+			{firstName} {lastName}
             </h3>
             <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
               <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
-              Los Angeles, California
+              Paris, France
             </div>
             <div className="mb-2 text-blueGray-600 mt-10">
-              <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>Solution Manager - Creative Tim Officer
+              <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>Semi-Professtional Pong Player - 42 Club
             </div>
             <div className="mb-2 text-blueGray-600">
-              <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>University of Computer Science
+              <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>42 Paris - Born2Code
             </div>
           </div>
           <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
             <div className="flex flex-wrap justify-center">
               <div className="w-full lg:w-9/12 px-4">
                 <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
-                  An artist of considerable range, Jenna the name taken by
-                  Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                  performs and records all of his own music, giving it a
-                  warm, intimate feel with a solid groove structure. An
-                  artist of considerable range.
+				Passionate online pong player with lightning-fast reflexes and a strategic mind. 
+				Consistently aiming for the perfect bounce and paddle precision. 
+				Join me in the virtual arena for intense matches and friendly competition!
                 </p>
                 <a href="#pablo" className="font-normal text-pink-500">Show more</a>
               </div>
@@ -106,7 +104,7 @@ function Profile() {
     <div className="flex flex-wrap items-center md:justify-between justify-center">
       <div className="w-full md:w-6/12 px-4 mx-auto text-center">
         <div className="text-sm text-blueGray-500 font-semibold py-1">
-          Made with <a href="https://www.creative-tim.com/product/notus-js" className="text-blueGray-500 hover:text-gray-800" target="_blank">Notus JS</a> by <a href="https://www.creative-tim.com" className="text-blueGray-500 hover:text-blueGray-800" target="_blank"> Creative Vit</a>.
+          Made with love by 42 Pooong Team
         </div>
       </div>
     </div>
