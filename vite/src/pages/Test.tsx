1caca90ceffcca3ff1eb6272589fa95dba058c79
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-08-28 20:29:08 +0200

    WIP Profile/Dash Board Remake

--- vite/src/pages/Test.tsx
+++ vite/src/pages/Test.tsx
@@ -27,21 +27,40 @@ function Test(props: { darkMode: boolean; toggleDarkMode: any }) {
 
                 <div className="flex h-screen bg-gray-800"
                 //  className={isSideMenuOpen ? 'overflow-hidden' : ''}
-                 >
+                >
                     <aside className="z-20 flex-shrink-0 hidden w-64 pl-2 overflow-y-auto bg-white dark:bg-pink-200 md:block">
-                    <div>
-                        <div className="text-white">
-                            <div className="flex p-2 bg-red-300">
-                                <div className="flex py-3 px-2 items-center">
-                                    <p className="text-2xl text-green-500 font-semibold">42</p>
-                                    " Pooong"
-                                    <p></p>
+                        <div>
+                            <div className="text-white">
+                                <div className="flex p-2 bg-red-300">
+                                    <div className="flex py-3 px-2 items-center">
+                                        <p className="text-2xl text-green-500 font-semibold">42</p>
+                                        " Pooong"
+                                        <p></p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
                     </aside>
                 </div>
+                <div className="flex justify-center">
+                    <div>
+                        <img className="hidden h-24 w-24 rounded-full sm:block object-cover mr-2 border-4 border-green-400" src={img} alt="Your avatar" />
+                        <p className="font-bold text-base text-gray-400 pt-2 text-center w-24"> {firstName} </p>
+                    </div>
+                </div>
+                <div>
+                    <ul className="mt-5 leading-10">
+                        <li className="relative px-2 py-1">
+                            <a className="inline-flex items-center w-full text-sm font-semibold text-white transition-colors duration-150 cursor-pointer hover:text-green-500  " href="#">
+                            //icon home
+                                <svg className="w-5 h-5" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2 2 2m6 7l-2-2-2 2m2-2v-6m2 18h6a2 2 0 002-2v-6m-2 8h-6a2 2 0 01-2-2v-6"></path>
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
                 <div className="flex bottom-0 left-0 right-0 z-40 px-4 py-3 text-center text-white bg-gray-800">
                     " Hello Vit "
                 </div>
