0506ef106dc515376626945f2048a002b2b6573a
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-08-28 20:17:01 +0200

    WIP Profile/Dash Board Remake

--- vite/src/pages/Test.tsx
+++ vite/src/pages/Test.tsx
@@ -4,28 +4,51 @@ import { getUser, handleLogout } from '../App';
 import Navbar from '../components/Navbar';
 
 function Test(props: { darkMode: boolean; toggleDarkMode: any }) {
-  const [firstName, setFirstName] = useState(String);
-  const [lastName, setLastName] = useState(String);
-  const [img, setImg] = useState(String);
+    const [firstName, setFirstName] = useState(String);
+    const [lastName, setLastName] = useState(String);
+    const [img, setImg] = useState(String);
 
-  useEffect(() => {
-    getUser().then((data) => {
-      setFirstName(data.firstName);
-      setLastName(data.lastName);
-      if (data.picture === null) {
-        setImg('../assets/duckie_bg_rm/sticker1.png');
-      } else {
-        setImg(data.picture);
-      }
-    });
-  }, []);
+    useEffect(() => {
+        getUser().then((data) => {
+            setFirstName(data.firstName);
+            setLastName(data.lastName);
+            if (data.picture === null) {
+                setImg('../assets/duckie_bg_rm/sticker1.png');
+            } else {
+                setImg(data.picture);
+            }
+        });
+    }, []);
 
-  return (
-    <>
-     <Navbar darkMode={props.darkMode} toggleDarkMode={props.toggleDarkMode} />
-    
-    </>
-  );
+    return (
+        <>
+            <Navbar darkMode={props.darkMode} toggleDarkMode={props.toggleDarkMode} />
+            <body className="bg-gray-200" data-new-gr-c-s-check-loaded="14.1121.0" data-gr-ext-installed="">
+
+                <div className="flex h-screen bg-gray-800"
+                //  className={isSideMenuOpen ? 'overflow-hidden' : ''}
+                 >
+                    <aside className="z-20 flex-shrink-0 hidden w-64 pl-2 overflow-y-auto bg-white dark:bg-pink-200 md:block">
+                    <div>
+                        <div className="text-white">
+                            <div className="flex p-2 bg-red-300">
+                                <div className="flex py-3 px-2 items-center">
+                                    <p className="text-2xl text-green-500 font-semibold">42</p>
+                                    " Pooong"
+                                    <p></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </aside>
+                </div>
+                <div className="flex bottom-0 left-0 right-0 z-40 px-4 py-3 text-center text-white bg-gray-800">
+                    " Hello Vit "
+                </div>
+
+            </body>
+        </>
+    );
 }
 
 export default Test;
