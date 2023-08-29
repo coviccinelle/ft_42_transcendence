f3581710d57e5dac46a0355ea5d4b8ed2852d1c6
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-08-29 13:43:14 +0200

    wip profile remake: navBar2 done with notification icon + config button + searchbar

--- vite/src/components/NavBar2.tsx
+++ vite/src/components/NavBar2.tsx
@@ -53,9 +53,9 @@ function Navbar2() {
 
                     {/* <!-- Notifications menu --> */}
                     <li className="relative">
-                        {/* <button
+                        <button
                                 className="p-2 bg-white text-green-400 align-middle rounded-full hover:text-white hover:bg-green-400 focus:outline-none "
-                                @click="toggleNotificationsMenu" @keydown.escape="closeNotificationsMenu"
+                                // @click="toggleNotificationsMenu" @keydown.escape="closeNotificationsMenu"
                                 aria-label="Notifications" aria-haspopup="true">
                                 <div className="flex items-cemter">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
@@ -64,34 +64,17 @@ function Navbar2() {
                                             d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                                     </svg>
                                 </div>
-                                <!-- Notification badge -->
+                                {/* <!-- Notification badge --> */}
                                 <span aria-hidden="true"
                                     className="absolute top-0 right-0 inline-block w-3 h-3 transform translate-x-1 -translate-y-1 bg-red-600 border-2 border-white rounded-full dark:border-gray-800"></span>
-                            </button> */}
-                        {/* <template x-if="isNotificationsMenuOpen">
-                                <ul x-transition:leave="transition ease-in duration-150"
-                                    x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-                                    @click.away="closeNotificationsMenu" @keydown.escape="closeNotificationsMenu"
-                                    className="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-green-400 border border-green-500 rounded-md shadow-md">
-                                    <li className="flex">
-                                        <a className="text-white inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800"
-                                            href="#">
-                                            <span>Messages</span>
-                                            <span
-                                                className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-600 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-600">
-                                                13
-                                            </span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </template> */}
+                            </button>
                     </li>
 
                     {/* <!-- Profile menu --> */}
                     <li className="relative">
-                        {/* <button
+                        <button
                                 className="p-2 bg-white text-green-400 align-middle rounded-full hover:text-white hover:bg-green-400 focus:outline-none "
-                                @click="toggleProfileMenu" @keydown.escape="closeProfileMenu" aria-label="Account"
+                                // @click="toggleProfileMenu" @keydown.escape="closeProfileMenu" aria-label="Account"
                                 aria-haspopup="true">
                                 <div className="flex items-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
@@ -102,37 +85,7 @@ function Navbar2() {
                                             d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                     </svg>
                                 </div>
-                            </button> */}
-                        {/* <template x-if="isProfileMenuOpen">
-                                <ul x-transition:leave="transition ease-in duration-150"
-                                    x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-                                    @click.away="closeProfileMenu" @keydown.escape="closeProfileMenu"
-                                    className="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-green-400 border border-green-500 rounded-md shadow-md"
-                                    aria-label="submenu">
-                                    <li className="flex">
-                                        <a className=" text-white inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800"
-                                            href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-2" fill="none"
-                                                viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                    d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                            </svg>
-                                            <span>Profile</span>
-                                        </a>
-                                    </li>
-                                    <li className="flex">
-                                        <a className="text-white inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800"
-                                            href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-2" fill="none"
-                                                viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                                            </svg>
-                                            <span>Log out</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </template> */}
+                            </button>
                     </li>
                 </ul>
 
