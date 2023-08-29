d1005e7d408aa5bd8027c931900bd491a40c5f86
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-08-29 13:28:43 +0200

    wip profile remake : smaller search bar

--- vite/src/components/NavBar2.tsx
+++ vite/src/components/NavBar2.tsx
@@ -26,7 +26,7 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
                         d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
             </span>
-            <button onClick={handleSearch} className="absolute right-0 top-0 mt-5 mr-4"> Search </button>
+            {/* <button onClick={handleSearch} className="absolute right-0 top-0 mt-5 mr-4"> Search </button> */}
         </div>
     )
 }
@@ -44,22 +44,10 @@ function Navbar2() {
                             d="M4 6h16M4 12h16M4 18h7" />
                     </svg>
                 </button>
-                <SearchBar onSearch={(query) => console.log(query)} />
-                {/* <!-- Search Input --> */}
-                {/* <div className="flex justify-center  mt-2 mr-4">
-                        <div className="relative flex w-full flex-wrap items-stretch mb-3">
-                            <input type="search" placeholder="Search" {{ $attributes }}
-                                className="form-input px-3 py-2 placeholder-gray-400 text-gray-700 relative bg-white rounded-lg text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full pr-10" />
-                            <span
-                                className="z-10 h-full leading-snug font-normal  text-center text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-8 right-0 pr-3 py-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 -mt-1" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                                </svg>
-                            </span>
-                        </div>
-                    </div> */}
+                {/* search bar */}
+                <div className="flex justify-center mt-2 mr-4">
+                    <SearchBar onSearch={(query) => console.log(query)} />
+                </div>
 
                 <ul className="flex items-center flex-shrink-0 space-x-6">
 
