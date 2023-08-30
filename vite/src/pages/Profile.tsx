04cdba429f08eb7c68e2613b3eb93aa4fc5527b8
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-08-30 14:15:38 +0200

    WIP: front Profile: online/offline + add friend button

--- vite/src/pages/Profile.tsx
+++ vite/src/pages/Profile.tsx
@@ -79,11 +79,8 @@ function Profile(props: { darkMode: boolean; toggleDarkMode: any }) {
                         <div>
                         <div className="w-full px-4 lg:order-3 lg:self-center">
                     <div className="py-6 px-3">
-                      <button className="bg-yellow-100 active:bg-green-300 uppercase text-black font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
+                      <button className="bg-yellow-100 active:bg-green-300 uppercase text-black font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded-2xl hover:text-blue-400 hover:animate-pulse sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
                         Add Friend
-                      </button>
-                      <button className="bg-yellow-100 active:bg-green-300 uppercase text-black font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
-                        Send Message
                       </button>
                     </div>
                   </div>
@@ -96,7 +93,7 @@ function Profile(props: { darkMode: boolean; toggleDarkMode: any }) {
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                                         </svg>
-                                        <span className="ml-4">Home onClick </span>
+                                        <a className="ml-4" href="/">Home</a>
                                     </a>
                                 </li>
                                 <li className="relative px-2 py-1" x-data="{ Open : false  }">
@@ -109,7 +106,7 @@ function Profile(props: { darkMode: boolean; toggleDarkMode: any }) {
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M8 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-2m-4-1v8m0 0l3-3m-3 3L9 8m-5 5h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 00.707.293h3.172a1 1 0 00.707-.293l2.414-2.414a1 1 0 01.707-.293H20" />
                                         </svg>
-                                        <span className="ml-4">ITEM</span>
+                                        <span className="ml-4">Send Message</span>
                                     </span>
                                 </div>
                                 </li>
