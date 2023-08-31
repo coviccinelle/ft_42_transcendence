1d9fcfc39a3ecd8a5ce0357c37f343b2768390d3
Hypermario <molvautj@gmail.com>
2023-08-31 13:09:06 +0200

    nickname in model now

--- nest/src/users/dto/create-user.dto.ts
+++ nest/src/users/dto/create-user.dto.ts
@@ -8,6 +8,12 @@ export class CreateUserDto {
   email: string;
 
   @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  @ApiProperty()
+  nickname: string;
+
+  @IsString()
   @ApiProperty()
   firstName: string;
 
