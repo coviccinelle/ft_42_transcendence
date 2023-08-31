923ecd24396880839597f39c70caddb3c1014d9b
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-08-31 20:39:33 +0200

    WIP Registration: photo change ok

--- vite/src/pages/Registration.tsx
+++ vite/src/pages/Registration.tsx
@@ -3,11 +3,8 @@ import apiUser from "../api/user";
 import { useNavigate } from 'react-router-dom';
 
 function Registration() {
-    //set up a default image for the profile picture and then allow the user to upload their own
-    //use the useState hook to set the default image
-    const [img, setImage] = useState(String);
+    const [img, setImage] = useState('');
     const navigate = useNavigate();
-
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -23,39 +20,37 @@ function Registration() {
             }
         };
         fetchUser();
+
     }, []);
 
-
+    // Handle file input change
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        const imageUrl = URL.createObjectURL(file);
+        setImage(imageUrl);
+    };
 
     return (
-        //a half way transparent div that takes up the whole screen
         <div className="flex flex-col items-center justify-center h-screen bg-amber-100 bg-opacity-30">
             <div className="flex flex-col items-center justify-center w-2/3 h-2/3 bg-transparent rounded-lg shadow-lg">
                 <p className="flex flex-col py-4 text-3xl text-orange-300 font-bold">Registration</p>
 
-                {/* //upload profile picture */}
                 <div className="grid grid-cols-2 gap-4 text-center justify-center">
-
                     <div className="flex flex-col justify-center items-center col-span-1 px-4 py-4 relative">
-                        <img src={img} alt="Default Image" className="justify-center items-center w-32 h-32 rounded-full  " />
-                        {/* <!-- Photo Upload Icon --> */}
-                        <label for="fileInput" class="bg-blue-500 text-white p-2 rounded-full absolute bottom-0 right-0 cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <img src={img} alt="Profile Image" className="w-32 h-32 rounded-full" />
+                        <label htmlFor="fileInput" className="bg-blue-500 text-white p-2 rounded-full absolute bottom-0 right-0 cursor-pointer">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
                             </svg>
                         </label>
-
-                        {/* <!-- Hidden File Input --> */}
-                        <input id="fileInput" type="file" class="hidden"/>
+                        <input id="fileInput" type="file" className="hidden" onChange={handleFileChange} />
                     </div>
-
-
-                <div className="col-span-1 px-4 relative border">
-                    Text text
+                    <div className="col-span-1 px-4 relative border">
+                        Text text
+                    </div>
                 </div>
             </div>
         </div>
-        </div >
     )
 }
 
