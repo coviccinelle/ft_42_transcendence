6bc2ef1d7f797c5dd6e56ffbe0f8a1fdf9494985
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-09-01 12:50:01 +0200

    WIP registration nickname + photo

--- vite/src/pages/Registration.tsx
+++ vite/src/pages/Registration.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 function Registration() {
     const [img, setImage] = useState('');
+    const [nickname, setNickname] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -32,21 +33,23 @@ function Registration() {
 
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-amber-100 bg-opacity-30">
-            <div className="flex flex-col items-center justify-center w-2/3 h-2/3 bg-transparent rounded-lg shadow-lg">
-                <p className="flex flex-col py-4 text-3xl text-orange-300 font-bold">Registration</p>
+            <div className="flex flex-col px-3 py-3 items-center justify-center w-4/5 h-4/5 bg-transparent rounded-lg shadow-lg">
+                <p className="flex flex-col py-16 font-bold animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl">Registration</p>
 
-                <div className="grid grid-cols-2 gap-4 text-center justify-center">
-                    <div className="flex flex-col justify-center items-center col-span-1 px-4 py-4 relative">
+                <div className="grid grid-cols-3 gap-4 text-center justify-center">
+
+                    <div className="cols-span-1 flex flex-col w-32 justify-center items-center px-1 py-1 relative">
                         <img src={img} alt="Profile Image" className="w-32 h-32 rounded-full" />
-                        <label htmlFor="fileInput" className="bg-blue-500 text-white p-2 rounded-full absolute bottom-0 right-0 cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
-                            </svg>
+                        <label htmlFor="fileInput" className="bg-amber-100 p-2 bg-opacity-80 rounded-full absolute bottom-0 right-0 cursor-pointer">
+                            📸
                         </label>
                         <input id="fileInput" type="file" className="hidden" onChange={handleFileChange} />
                     </div>
-                    <div className="col-span-1 px-4 relative border">
-                        Text text
+
+
+                    <div className="col-span-2 px-4 py-4 relative ">
+                       <p className="text-center text-orange-200 font-bold py-2" >Your unique nickname*</p>
+                        <input type="text" className="w-full px-4 py-2 border rounded-lg" />
                     </div>
                 </div>
             </div>
