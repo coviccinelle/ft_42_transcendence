7cb04dfc4adc59e32cfb7a26750fb5a1ea552086
Thi phuong thao Nguyen <thi-phng@c2r2s7.forty2.fr>
2023-09-05 16:51:30 +0200

    Profile others: idk why it's not working

--- vite/src/components/profile/UserProfile.tsx
+++ vite/src/components/profile/UserProfile.tsx
@@ -1,41 +1,38 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import apiUser from '../../api/user';
-import User from '../../api/user';
 import UserList from '../UserList';
 
 
 function UserProfile() {
-    const { id } = useParams();
+// show user profile based on userId
+    const { id } = useParams<{ id: string }>();
     const [userData, setUserData] = useState<{ name: string} | undefined>(undefined);
 
     // fetch user data based on userId
     useEffect(() => {
-      console.log('userId', id);
+        console.log('userId', id);
+        console.log('userData', userData);
         const fetchUser = async () => {
             const users = await apiUser.getUsers();
             setUserData(users.find((user: any) => user.id === id));
         };
         fetchUser();
-    }, [id]);
+    }
+    , [id]);
 
     return (
         <div>
           <UserList />
-      {/* Display user data in the profile */}
-      <h1>User Profile for [{userData?.name}] hihi</h1>
-      {/* Add conditional rendering for buttons */}
-      {id !== 'yourUserId' && (
-        <div>
-          {/* Add Friend button */}
-          <button>Add Friend</button>
-          {/* Send Message button */}
-          <button>Send Message</button>
+            <h1>User Profile</h1>
+            <p>{id}</p>
+            <p>User name : [</p>
+            <p>{userData?.name}]</p>
         </div>
-        // a list
-      )}
-    </div>
     );
+
+
+
 }
 
 export default UserProfile;
