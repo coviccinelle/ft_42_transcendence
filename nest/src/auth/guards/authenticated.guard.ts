20a994b28aaaccb752677f5ae0b56452235aa64b
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2023-09-12 14:07:43 +0200

    Auth backend: Authenticated guard handles websocket connections

--- nest/src/auth/guards/authenticated.guard.ts
+++ nest/src/auth/guards/authenticated.guard.ts
@@ -3,7 +3,13 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
   async canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-    return request.isAuthenticated();
+    if (context.getType() === 'http') {
+      const request = context.switchToHttp().getRequest();
+      return request.isAuthenticated();
+    } else if (context.getType() === 'ws') {
+      const request = context.getArgByIndex(0);
+      return !!request.data.user;
+    }
+    return false;
   }
 }
