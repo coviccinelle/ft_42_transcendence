94a94042a35636188b12c2029fd3f55c9130a02a
Thi Phuong Thao NGUYEN <thi-phng@student.42.fr>
2023-09-13 09:19:55 +0200

    Update front for the Enable-2fa page

--- vite/src/pages/Enable-2fa.tsx
+++ vite/src/pages/Enable-2fa.tsx
@@ -64,24 +64,36 @@ function EnableTwoFA() {
   }
 
   return (
-    <div className="flex h-screen w-screen justify-center items-center">
-      <img src={image} alt="Image" />
+    <div className="flex h-screen w-screen justify-center items-center text-center">
+      <div className="flex flex-col items-center bg-white rounded-lg shadow-lg p-6 bg-opacity-5 relative">
+        <button
+          className="absolute top-0 right-0 mt-4 mr-4 m-5 w-8 h-8 bg-yellow-300 bg-opacity-40 rounded-xl hover:rounded-3xl hover:bg-yellow-600 transition-all duration-300 text-white"
+          onClick={() => navigate('/profile')}
+        >
+          X
+        </button>
+      <img src={image} alt="Image" className="w-24 h-24 mb-8" />
       <div className="user-box">
-        <label>Enter The 2FA Code To Verify Association</label>
+        <label className="text-gray-200 py-4 text-lg font-semibold ">
+          Enter The 2FA Code To Verify Association
+        </label>
         <input
-          type="code"
+          type="text"
           value={code}
           onChange={(e) => setCode(e.target.value)}
+          className="w-1/2 mt-2 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-amber-500 focus:border-amber-500"
         />
       </div>
       <button
-        className="mx-3 px-2 py-2 rounded-md border border-transparent cursor-pointer hover:border-amber-200 focus:outline-4 focus:ring-amber-500 focus:border-amber-500 focus:ring-opacity-50"
+        className="mt-12 px-4 py-2 rounded-md bg-amber-500 text-white hover:bg-amber-600 focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-opacity-50"
         type="button"
         onClick={handleTurnOnTwoFA}
       >
         Activate 2FA
       </button>
     </div>
+    </div >
+
   );
 }
 
